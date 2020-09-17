f79ea5d0541d2464c781fc3e268aab3a3cefa179
Gabriel Menino <grmenino@gmail.com>
2020-09-17 20:55:14 -0300

    First commit

---
+++ src/main/java/com/menino/pokedexapi/PokedexApi1Application.java
@@ -0,0 +1,13 @@
+package com.menino.pokedexapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexApi1Application.class, args);
+	}
+
+}
