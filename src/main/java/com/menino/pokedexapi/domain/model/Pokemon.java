ed4278928665536f6007fcdef911405d5f7f93c8
Gabriel Machado <gabriel.machado@ifood.com.br>
2020-09-19 23:05:26 -0300

    Add lombok and remove image property from pokemon

--- src/main/java/com/menino/pokedexapi/domain/model/Pokemon.java
+++ src/main/java/com/menino/pokedexapi/domain/model/Pokemon.java
@@ -1,15 +1,16 @@
 package com.menino.pokedexapi.domain.model;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Entity
+@Data
 public class Pokemon {
 
-	@NotBlank
-	private String imagem;
 	@Id
 	@NotNull
 	private int numero;
@@ -21,44 +22,5 @@ public class Pokemon {
 	private String tipo1;
 	
 	private String tipo2;
-	
-	
-	//Getters e setters
 
-	public String getImagem() {
-		return imagem;
-	}
-	public void setImagem(String imagem) {
-		this.imagem = imagem;
-	}
-	public int getNumero() {
-		return numero;
-	}
-	public void setNumero(int numero) {
-		this.numero = numero;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	public String getTipo1() {
-		return tipo1;
-	}
-	public void setTipo1(String tipo1) {
-		this.tipo1 = tipo1;
-	}
-	public String getTipo2() {
-		return tipo2;
-	}
-	public void setTipo2(String tipo2) {
-		this.tipo2 = tipo2;
-	}
 }
